f4e1f25eb67eac8d43955a5c1e119c5012dace63
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2025-11-21 22:53:56 -0300

    feat: auditing toi

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as UseAuditable;
 
-class Location extends Model
+class Location extends Model implements Auditable
 {
     /** @use HasFactory<\Database\Factories\LocationFactory> */
-    use HasFactory;
+    use HasFactory, UseAuditable;
 
     protected $table = 'locations';
 
--- app/Models/Material.php
+++ app/Models/Material.php
@@ -5,13 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use OwenIt\Auditing\Contracts\Auditable;
-
+use OwenIt\Auditing\Auditable as UseAuditable;
 
 class Material extends Model implements Auditable
 {
     /** @use HasFactory<\Database\Factories\MaterialFactory> */
-    use HasFactory;
-    use \OwenIt\Auditing\Auditable;
+    use HasFactory, UseAuditable;
 
     protected $table = 'materials';
 
